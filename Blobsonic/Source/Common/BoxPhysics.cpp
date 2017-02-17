ffa09aa49a949b2de5399a074e0c0b9a42c4e201
Chris <chris.s.oneill@o2.co.uk>
2017-02-17 14:35:44 +0000

    Physics test begin

--- Blobsonic/Source/Common/BoxPhysics.cpp
+++ Blobsonic/Source/Common/BoxPhysics.cpp
@@ -31,25 +31,25 @@ BoxPhysics::BoxPhysics(Model model)
 void BoxPhysics::update(const float dt)
 {
 
-	m_vAcceleration.y = GRAVITYCOEFFICENT;
+	//m_vAcceleration.y = GRAVITYCOEFFICENT;
 
-	//suvat for falling
-	glm::vec3 s = m_vPosition;
-	glm::vec3 u = m_fVelocity;
-	glm::vec3 v = vec3(0,0,0);
-	glm::vec3 a = m_vAcceleration;
-	
-	//eular calculation
-	v = u + a * dt;
-	glm::vec3 s2 = s + u * dt;
+	////suvat for falling
+	//glm::vec3 s = m_vPosition;
+	//glm::vec3 u = m_fVelocity;
+	//glm::vec3 v = vec3(0,0,0);
+	//glm::vec3 a = m_vAcceleration;
+	//
+	////eular calculation
+	//v = u + a * dt;
+	//glm::vec3 s2 = s + u * dt;
 
-	//update position
-	m_vPosition = s2;
-	m_fVelocity = v;
-	
-	std::cout << "Update physics box : " << m_vPosition.y << " " << m_fVelocity.y << " " << m_vPosition.y << std::endl;
+	////update position
+	//m_vPosition = s2;
+	//m_fVelocity = v;
+	//
+	//std::cout << "Update physics box : " << m_vPosition.y << " " << m_fVelocity.y << " " << m_vPosition.y << std::endl;
 
-	m_RenderModel.setPosition(m_vPosition);
+	//m_RenderModel.setPosition(m_vPosition);
 
 }
 
@@ -65,12 +65,41 @@ void BoxPhysics::setLocalMsgPtr(std::vector<std::shared_ptr<Message>>* ptr)
 {
 }
 
-void BoxPhysics::CollideWithBox(BoxPhysics & other)
+void BoxPhysics::CollideWithBox(BoxPhysics* other)
 {
+	
+	//setup data for the OBB's
 	m_vCenter = m_vPosition;
 	m_vExtends = m_vScale / 2.0f;
-	
 
+	other->m_vCenter = other->m_vPosition;
+	other->m_vExtends = other->m_vScale / 2.0f;
+
+	//perform an early out check 
+	glm::vec3 earlyOutDist(m_vCenter - other->m_vCenter); // distance between center of 2 boxs 
+	float magEarlyDist = sqrt(((earlyOutDist.x * earlyOutDist.x) + (earlyOutDist.y * earlyOutDist.y) + (earlyOutDist.z * earlyOutDist.z))); // get magnitude of dist
+	float roughSize = m_vExtends.x + m_vExtends.y + m_vExtends.z; // create a rough size from the scale of the box
+	std::cout <<"Rough Size : " << roughSize << " magEarlyDist : " << magEarlyDist << std::endl;
+	if (magEarlyDist > roughSize * 10.0f) // if further than 10 times the rouch size of the box away
+	{
+		
+		return; // escape collision test before any intensive testing begins
+	}
+	else
+	{
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 
 }
 
