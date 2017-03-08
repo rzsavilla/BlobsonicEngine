fa7cc88ffc1f371a393b99630953bde2314eaf02
Cristian <P16000638@GWHXX633WIN013W>
2017-03-08 19:25:17 +0000

    didnt work last time. Setting MVC model on editor

--- Editor/Project1/EditorController.cpp
+++ Editor/Project1/EditorController.cpp
@@ -2,10 +2,9 @@
 #include "EditorData.h"
 #include "EditorView.h"
 
-
-
 EditorController::EditorController()
 {
+
 	eView = new EditorView();
 	eData = new EditorData();
 	eView->Init();
@@ -14,4 +13,18 @@ EditorController::EditorController()
 
 EditorController::~EditorController()
 {
+}
+
+void EditorController::addScene(std::string ID, Scene newScene) {
+	m_vScene.push_back(std::pair<std::string, Scene>(ID, newScene));
+}
+
+Scene* EditorController::getScene(std::string ID) {
+	//Look through vector
+	for (auto it = m_vScene.begin(); it != m_vScene.end(); ++it) {
+		if ((*it).first == ID) {
+			return &(*it).second;		//Returns texture pointer
+		}
+	}
+	return NULL;
 }
--- Editor/Project1/EditorController.h
+++ Editor/Project1/EditorController.h
@@ -1,17 +1,22 @@
+#include "Scene.h"
+
 #pragma once
 class EditorView;
 class EditorData;
+
 class EditorController
 {
+private:
+	std::vector<std::pair<std::string, Scene>> m_vScene;
+
 public:
 	EditorController();
 	~EditorController();
 	EditorView* eView;
 	EditorData* eData;
 
-	struct st_Project {
-
-	};
+	void addScene(std::string ID, Scene newScene);
+	Scene* getScene(std::string ID);
 
 };
 
