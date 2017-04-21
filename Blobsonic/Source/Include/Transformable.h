e4e23059081f2774df120c77203eddde934e11af
rzsavilla <rzsavilla@googlemail.com>
2017-04-21 09:47:57 +0100

    Removed Unused Variable
    Removed matrix and orientation quat public variables.

--- Blobsonic/Source/Include/Transformable.h
+++ Blobsonic/Source/Include/Transformable.h
@@ -12,13 +12,9 @@
 namespace Component {
 	struct Transformable : public Component {
 		Transformable() {
-			//m_transform = glm::mat4(1.0f);
 			m_fSpeed = 5.0f;
 			m_fRotationSpeed = 0.1f;
-			m_orienation = glm::quat(0.0f, 1.0f, 1.0f, 0.0f);
 		};
-		//glm::mat4 m_transform;
-		glm::quat m_orienation;
 		float m_fSpeed;
 		float m_fRotationSpeed;
 
