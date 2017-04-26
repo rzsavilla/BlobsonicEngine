845955fa32dc7d531f398549d0b026b97da46296
rzsavilla <rzsavilla@googlemail.com>
2017-04-26 16:57:18 +0100

    Directional Light Component

---
+++ Blobsonic/Source/Common/DirectionalLight.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "DirectionalLight.h"
+
+Component::DirectionalLight::DirectionalLight()
+{
+
+}
---
+++ Blobsonic/Source/Include/DirectionalLight.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Component.h"
+
+namespace Component {
+	class DirectionalLight :public Component {
+	private:
+
+	public:
+		DirectionalLight();
+	};
+}
