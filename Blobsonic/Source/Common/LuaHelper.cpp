04186eba2cf09db955734d9b5eb20d958af78b7a
rzsavilla <rzsavilla@googlemail.com>
2017-05-01 16:59:32 +0100

    Read Vec3 and Vec2 from sol::table

--- Blobsonic/Source/Common/LuaHelper.cpp
+++ Blobsonic/Source/Common/LuaHelper.cpp
@@ -12,3 +12,56 @@ void LuaHelper::loadScriptFile(lua_State * L, std::string filename)
 	sol::state_view lua(L);
 	lua.script_file(filename);
 }
+
+glm::vec2 LuaHelper::readVec2(sol::object o)
+{
+	//Convert object to table
+	sol::table t = o.as<sol::table>();
+	//Read table variables
+	return readVec2(t);
+}
+
+glm::vec3 LuaHelper::readVec3(sol::object o)
+{
+	//Convert object to table
+	sol::table t = o.as<sol::table>();
+	//Read table variables
+	return readVec3(t);
+}
+
+glm::vec2 LuaHelper::readVec2(sol::table t)
+{
+	float fValue;
+	glm::vec2 v;
+	//Iterate through table elements
+	for (auto it = t.begin(); it != t.end(); ++it) {
+		auto key = (*it).first;	//Get element key
+		//Processes ony strings
+		if (key.get_type() == sol::type::string) {
+			std::string s = key.as<std::string>();		//Key
+			auto value = (*it).second.as<float>();
+			if (s == "x") v.x = value;
+			else if (s == "y")	v.y = value;
+		}
+	}
+	return v;
+}
+
+glm::vec3 LuaHelper::readVec3(sol::table t)
+{
+	float fValue;
+	glm::vec3 v;
+	//Iterate through table elements
+	for (auto it = t.begin(); it != t.end(); ++it) {
+		auto key = (*it).first;	//Get element key
+		//Processes ony strings
+		if (key.get_type() == sol::type::string) {
+			std::string s = key.as<std::string>();		//Key
+			auto value = (*it).second.as<float>();
+			if (s == "x") v.x = value;
+			else if (s == "y")	v.y = value;
+			else if (s == "z")	v.z = value;
+		}
+	}
+	return v;
+}
--- Blobsonic/Source/Include/LuaHelper.h
+++ Blobsonic/Source/Include/LuaHelper.h
@@ -5,4 +5,9 @@
 namespace LuaHelper {
 	void loadLibraries(lua_State* L);
 	void loadScriptFile(lua_State* L, std::string filename);
+
+	glm::vec2 readVec2(sol::object o);
+	glm::vec3 readVec3(sol::object o);
+	glm::vec2 readVec2(sol::table t);
+	glm::vec3 readVec3(sol::table t);
 }
