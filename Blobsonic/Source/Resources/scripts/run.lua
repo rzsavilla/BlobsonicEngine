f48b707b49df175598fc70fe4569a820ded371e3
Cristian <cristianss_66@hotmail.com>
2017-05-06 15:26:20 +0100

    Models
    Added models to clone repository

--- Blobsonic/Source/Resources/scripts/run.lua
+++ Blobsonic/Source/Resources/scripts/run.lua
@@ -1,36 +1,29 @@
 -- This is where the game logic is updated and is run every game update
 
 
-fSpeed = 500
-
+fSpeed = 1000
 if isKeyDown("p") == true then
 		reloadScene()
-end
-
+	end
+	
 if getActiveScene() == "WorldTest.xml" then
-	if isKeyDown("w") == true then
-		player:pSetVelocity(0,0,-fSpeed)
-	elseif isKeyDown("s") == true then
-		player:pSetVelocity(0,0,fSpeed)
-	elseif isKeyDown("a") == true then
-		player:pSetVelocity(-fSpeed,0,0)
-	elseif isKeyDown("d") == true then
-		player:pSetVelocity(fSpeed,0,0)
-	elseif isKeyDown("space") == true then
-		player:pSetVelocity(0,fSpeed,0)
+
+	
+
+	if isKeyDown("up") == true then
+		--player:tSetPosition(0,1000,0)
+		--player:destroy()
+	elseif isKeyDown("down") == true then
+		--player:pSetVelocity(0,-fSpeed,0)
+	elseif isKeyDown("left") == true then
+		--player:pSetVelocity(-fSpeed,0,0)
+	elseif isKeyDown("right") == true then
+		--player:pSetVelocity(fSpeed,0,0)
 	end
 
-	if player:tGetPosY() < 0 then
-		player:tSetPosition(player:tGetPosX(),0,player:tGetPosZ())
-	end
 
 	if player:pHasCollidedByName(floorHandle:getName()) == true then
-
-	end
-
-	if player:pHasCollidedByName("pickup") == true then
-		reloadScene()
-		player:log()
+		player:tSetPosition(0,1000,0)
 	end
 
 end
